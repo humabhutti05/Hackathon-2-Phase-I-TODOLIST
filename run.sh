527498570a511ed7e57641fe664ce99a9d8cf127
Huma Bhatti <humabhutti378@gmail.com>
2026-02-08 05:59:36 +0500

    Cleanup: Remove project-internal files from tracking

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-PYTHONPATH=. ~/.local/bin/uv run python src/main.py "$@"
--- test_imports.py
+++
@@ -1,9 +0,0 @@
-print("Testing src.todo.models...")
-from src.todo.models import Task
-print("Testing src.todo.storage...")
-from src.todo.storage import InMemoryTaskStorage
-print("Testing src.todo.service...")
-from src.todo.service import TodoService
-print("Testing src.manager...")
-from src.manager import TodoManager
-print("All imports successful")
